b0d6361940b7ab49fa8305c53cb7abc14f03fb92
Gabriel Mazuru <gabrielmazuru6@gmail.com>
2025-07-29 13:25:28 +0300

    some changes

--- src/components/CreateArt/CreateArt.jsx
+++ src/components/CreateArt/CreateArt.jsx
@@ -1,10 +1,33 @@
 import styles from "./CreateArt.module.css";
+import { useState } from "react";
 
 function CreateArt() {
+  const [art, setArt] = useState(null);
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setArt(URL.createObjectURL(file));
+    }
+  };
+
   return (
-    <div>
+    <div className={styles.container}>
       <h1>Create Artwork</h1>
-      <input type="image" />
+
+      <label className={styles.uploadLabel}>
+        <input
+          type="file"
+          accept="image/*"
+          className={styles.inputFile}
+          onChange={handleImageChange}
+        />
+      </label>
+
+      {art && <img src={art} alt="Preview" className={styles.previewImage} />}
+
+      <input type="text" placeholder="description" />
+      <button>Submit</button>
     </div>
   );
 }
